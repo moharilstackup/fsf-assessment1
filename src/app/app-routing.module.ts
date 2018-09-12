450e06d1fed545ec1d3f17c0200fc2355402523d
Mohd Shahril <moharilstackup@gmail.com>
2018-09-12 16:37:00 +0800

    make sure all changes commited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { ThankyouComponent } from './components/thankyou.component';
 import { PageNotFoundComponent } from './components/page-not-found.component';
 
 const routes : Routes = [
-    { path: '', redirectTo: '/registrationForm', pathMatch: 'full' },
+    // { path: '', redirectTo: '/registrationForm', pathMatch: 'full' },
+    { path: '', component: RegistrationFormComponent },
     { path: 'registrationForm', component : RegistrationFormComponent },
     { path: 'thankyou' , component : ThankyouComponent},
     { path: '**' , component : PageNotFoundComponent}
